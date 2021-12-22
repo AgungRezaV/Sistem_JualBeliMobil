4f74db367ae9e7725edbed3dfdbf32f1144ce962
Nadyazhr <nadyazrr@gmail.com>
2021-12-22 11:26:16 +0700

    Ganti nama Form Test 1 menjadi FormLogin

--- SistemJualBeliMobil_Kelompok2/FormLogin.Designer.vb
+++ SistemJualBeliMobil_Kelompok2/FormLogin.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Test1
+Partial Class FormLogin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
