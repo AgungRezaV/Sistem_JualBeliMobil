51c94957d307fe692a68021ded82416756f69472
maudyalimy <maudyalimy9@gmail.com>
2022-01-10 21:55:07 +0700

    edit form jenis mobil

--- SistemJualBeliMobil_Kelompok2/JenisMobil.Designer.vb
+++ SistemJualBeliMobil_Kelompok2/JenisMobil.Designer.vb
@@ -22,9 +22,66 @@ Partial Class JenisMobil
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.DG = New System.Windows.Forms.DataGridView()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.BtnTambah = New System.Windows.Forms.Button()
+        CType(Me.DG, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DG
+        '
+        Me.DG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DG.Location = New System.Drawing.Point(12, 12)
+        Me.DG.Name = "DG"
+        Me.DG.Size = New System.Drawing.Size(578, 221)
+        Me.DG.TabIndex = 0
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Location = New System.Drawing.Point(515, 243)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.BtnHapus.TabIndex = 1
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.Location = New System.Drawing.Point(434, 243)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEdit.TabIndex = 2
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
+        '
+        'BtnTambah
+        '
+        Me.BtnTambah.Location = New System.Drawing.Point(353, 243)
+        Me.BtnTambah.Name = "BtnTambah"
+        Me.BtnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.BtnTambah.TabIndex = 3
+        Me.BtnTambah.Text = "Tambah"
+        Me.BtnTambah.UseVisualStyleBackColor = True
+        '
+        'JenisMobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(602, 279)
+        Me.Controls.Add(Me.BtnTambah)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.DG)
+        Me.Name = "JenisMobil"
         Me.Text = "JenisMobil"
+        CType(Me.DG, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents DG As DataGridView
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents BtnTambah As Button
 End Class
---
+++ SistemJualBeliMobil_Kelompok2/TambahJenisMobil.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TambahJenisMobil
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblJenis = New System.Windows.Forms.Label()
+        Me.TxtboxJenis = New System.Windows.Forms.TextBox()
+        Me.BtnTmbhJenis = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblJenis
+        '
+        Me.lblJenis.AutoSize = True
+        Me.lblJenis.Font = New System.Drawing.Font("Maiandra GD", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJenis.Location = New System.Drawing.Point(23, 82)
+        Me.lblJenis.Name = "lblJenis"
+        Me.lblJenis.Size = New System.Drawing.Size(111, 19)
+        Me.lblJenis.TabIndex = 0
+        Me.lblJenis.Text = "Jenis Mobil :"
+        '
+        'TxtboxJenis
+        '
+        Me.TxtboxJenis.Location = New System.Drawing.Point(213, 83)
+        Me.TxtboxJenis.Name = "TxtboxJenis"
+        Me.TxtboxJenis.Size = New System.Drawing.Size(243, 20)
+        Me.TxtboxJenis.TabIndex = 1
+        '
+        'BtnTmbhJenis
+        '
+        Me.BtnTmbhJenis.Location = New System.Drawing.Point(381, 143)
+        Me.BtnTmbhJenis.Name = "BtnTmbhJenis"
+        Me.BtnTmbhJenis.Size = New System.Drawing.Size(75, 23)
+        Me.BtnTmbhJenis.TabIndex = 2
+        Me.BtnTmbhJenis.Text = "Tambah"
+        Me.BtnTmbhJenis.UseVisualStyleBackColor = True
+        '
+        'TambahJenisMobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 207)
+        Me.Controls.Add(Me.BtnTmbhJenis)
+        Me.Controls.Add(Me.TxtboxJenis)
+        Me.Controls.Add(Me.lblJenis)
+        Me.Name = "TambahJenisMobil"
+        Me.Text = "TambahJenisMobil"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblJenis As Label
+    Friend WithEvents TxtboxJenis As TextBox
+    Friend WithEvents BtnTmbhJenis As Button
+End Class
